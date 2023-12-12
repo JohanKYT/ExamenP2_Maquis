44642994370d3f1823fdbc8b1538c18af277a0ab
Kevin Maquis <kevin.maquis@udla.edu.ec>
2023-12-12 15:35:06 -0500

    Se hace la primera funcion y se define valores predeterminados

--- Progreso2examen.c
+++ Progreso2examen.c
@@ -3,6 +3,23 @@
 #include <string.h>
 #include <time.h>
 
+#define m 30
+#define n 12
+#define MAX_LONGITUD_NOMBRE 50
+
+// Función para inicializar datos con números aleatorios
+void inicializarDatos(char nombre[m][MAX_LONGITUD_NOMBRE], int ingresos[m][n], int egresos[m][n]) {
+    for (int i = 0; i < m; i++) {
+        printf("Ingrese el nombre del Insumo %d: ", i + 1);
+        scanf("%s", nombre[i]);
+
+        for (int j = 0; j < n; j++) {
+            ingresos[i][j] = rand() % RAND_MAX;  // Números aleatorios entre 0 y 999
+            egresos[i][j] = rand() % ingresos[i][j];   // Números aleatorios entre 0 y el valor de ingresos
+        }
+    }
+}
+
 
 
 
